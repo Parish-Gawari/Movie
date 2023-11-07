818876b1f5e8bf1b1b88d9e51a609fc60b200e91
Samael-24 <parishk1024@gmail.com>
2023-11-07 18:32:07 +0530

    did some chnages in home.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import "react-alice-carousel/lib/alice-carousel.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
